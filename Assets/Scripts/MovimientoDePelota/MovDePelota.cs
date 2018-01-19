3cde32d0e56530b87160bbed95b7b35c25875cc4
Ngakk <ngakwastaken@gmail.com>
2018-01-19 14:46:25 -0600

    Merge de hoyo 1 y 2, inicio de scripts

---
+++ Assets/Scripts/MovimientoDePelota/MovDePelota.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MovDePelota : MonoBehaviour {
+
+	Vector3 Direction; 	//Vector unitario de direccion
+	float Fuerza;		//Magnitud del vector
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
