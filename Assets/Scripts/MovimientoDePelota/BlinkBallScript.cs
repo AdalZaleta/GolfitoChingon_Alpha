b3eccd998ff97c04bc1f323bd108600e1f026ad3
Ngakk <ngakwastaken@gmail.com>
2018-02-03 21:17:28 -0600

    Inicio de BlinkBall

---
+++ Assets/Scripts/MovimientoDePelota/BlinkBallScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BlinkBallScript : MonoBehaviour {
+
+	public float BlinkDistance;
+	public KeyCode Activate;
+	Rigidbody rigi;
+	// Use this for initialization
+	void Start () {
+		rigi = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (Activate)) {
+			transform.Translate (rigi.velocity.normalized.x * BlinkDistance,
+				rigi.velocity.normalized.y * BlinkDistance,
+				rigi.velocity.normalized.z * BlinkDistance,
+				Space.World); 
+			
+		}
+	}
+}
