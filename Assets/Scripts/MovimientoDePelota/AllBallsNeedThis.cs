3535cbb41857c975e62f571e4ed6dea0a8e181e8
Galaxyben <galaxyben1@gmail.com>
2018-02-04 10:20:05 -0600

    Anexo SFX
    dadjwodiajwoidaw

--- Assets/Scripts/MovimientoDePelota/AllBallsNeedThis.cs
+++ Assets/Scripts/MovimientoDePelota/AllBallsNeedThis.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class AllBallsNeedThis : MonoBehaviour {
-
+	public AudioSource AS;
+	[Tooltip("SFX cuando choque con Pared")]
+	public AudioClip ChoqueParedSFX;
+	[Tooltip("SFX cuando choca con otra Pelota")]
+	public AudioClip ChoquePelotaSFX;
 	public float invulerabilityTime;
 	public bool triggerInvBlink = false;
 	public bool beingAttractedExpl = true;
 	public bool isWating;
 	public KeyCode Activate;
+
+	void Start()
+	{
+		AS = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter (Collision _col)
+	{
+		if (_col.gameObject.CompareTag ("BlinkBall")) {
+			AS.PlayOneShot (ChoquePelotaSFX);
+		}
+		if (_col.gameObject.CompareTag ("GavityBall")) {
+			AS.PlayOneShot (ChoquePelotaSFX);
+		}
+		if (_col.gameObject.CompareTag ("ExplotionBall")) {
+			AS.PlayOneShot (ChoquePelotaSFX);
+		}
+	}
+
 	public void BlinkInv (){
 		triggerInvBlink = true;
 	}
