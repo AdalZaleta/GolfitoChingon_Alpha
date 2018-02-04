953317330567c58e361942a8eae9f77afd41d776
Galaxyben <galaxyben1@gmail.com>
2018-02-04 08:42:36 -0600

    UI pt.3
    Súper ultra mega avance. Casi terminado

---
+++ Assets/Scrits/SceneSwitch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour {
+	public int characterSelected;
+	public void LoadGameStarter(int sceneStarter )
+	{
+		SceneManager.LoadScene (sceneStarter);
+	}
+}
