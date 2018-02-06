3f03863bb413f7af678ec5778e3fa839f7f36cde
Galaxyben <galaxyben1@gmail.com>
2018-02-06 13:25:27 -0600

    WindBoxes para hoyo 4 si no mal recuerdo

---
+++ Assets/Scripts/MovimientoDePelota/WindBoxxes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindBoxxes : MonoBehaviour {
+
+	public float Fuerza;
+
+	void OnTriggerStay(Collider _col){
+		if (_col.CompareTag ("Gball")) {
+			_col.GetComponent<Rigidbody> ().AddForce (Vector3.left * Time.deltaTime * Fuerza, ForceMode.Force);
+		}
+	}
+}
