94d3b39dc5f6427dc79fe6660b18757e9c7b91b2
Galaxyben <galaxyben1@gmail.com>
2018-02-07 15:03:27 -0600

    Anexo de Sonidos (Death Commit)
    101001000101110101

---
+++ Assets/SoundScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundScript : MonoBehaviour {
+	public AudioSource AS;
+	public AudioClip Music;
+
+	void Start () {
+		AS.GetComponent<AudioSource> ();
+		AS.PlayOneShot (Music);
+	}
+}
